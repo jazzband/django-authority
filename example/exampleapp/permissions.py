ff0150398ca94726eb77b64a9c40ac8382c5f1d6
Jannis Leidel <jannis@leidel.info>
2009-06-12 01:12:51 +0200

    Updated the FlatPage permissions to work with the latest changes. Added a custom permission check 'top_secret'. Added more examples to flatpage template.

--- example/exampleapp/permissions.py
+++ example/exampleapp/permissions.py
@@ -1,6 +1,15 @@
 from django.contrib.flatpages.models import FlatPage
-from authority.permissions import BasePermission
+from django.utils.translation import ugettext_lazy as _
 
-class FlatPagePermission(BasePermission):
+from authority import permissions
+
+class FlatPagePermissionSet(permissions.BasePermission):
     model = FlatPage
     label = 'flatpage_permission'
+    checks = ('top_secret',)
+
+    def top_secret(self, flatpage=None):
+        if flatpage and flatpage.registration_required:
+            return self.can_browse(obj=flatpage)
+        return False
+    top_secret.verbose_name=_('Is allowed to see top secret flatpages')
