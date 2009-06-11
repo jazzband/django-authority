6163e02b26e7c307701719df12559967175ea53b
Jannis Leidel <jannis@leidel.info>
2009-06-12 01:22:43 +0200

    A short description for the example FlatPagePermission

--- example/exampleapp/permissions.py
+++ example/exampleapp/permissions.py
@@ -3,7 +3,13 @@ from django.utils.translation import ugettext_lazy as _
 
 from authority import permissions
 
-class FlatPagePermissionSet(permissions.BasePermission):
+class FlatPagePermission(permissions.BasePermission):
+    """
+    This class contains a bunch of default and one custom permission check.
+    You can use the default checks can_add, can_browse, can_change and
+    can_delete to assemble your own. They can later be checked against from
+    your views directly or templates with the ifhasperm template tag.
+    """
     model = FlatPage
     label = 'flatpage_permission'
     checks = ('top_secret',)
