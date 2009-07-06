8aceec210476f0565585bc66fe556a0855f2d388
Jannis Leidel <jannis@leidel.info>
2009-07-06 16:07:24 +0200

    Updated example for permission_required_or_403 decorator to follow new syntax.

--- example/exampleapp/views.py
+++ example/exampleapp/views.py
@@ -3,7 +3,8 @@ from django.contrib.flatpages.models import FlatPage
 
 from authority.decorators import permission_required, permission_required_or_403
 
-#@permission_required_or_403('flatpage_permission.top_secret', (FlatPage, 'url__contains')) # use this to return a 403 page
+# @permission_required_or_403('flatpage_permission.top_secret', { # use this to return a 403 page
+#     'url': (FlatPage, 'url__contains'), 'lala': (FlatPage, 'url__contains')})
 @permission_required('flatpage_permission.top_secret', {
     'url': (FlatPage, 'url__contains'), 'lala': (FlatPage, 'url__contains')})
 def top_secret(request, url, lala=None):
