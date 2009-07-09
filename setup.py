991905311f8fcc3af55b89e013f8faf6976dba54
Jannis Leidel <jannis@leidel.info>
2009-07-10 01:09:42 +0200

    Updated setup.py and manifest template

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-authority',
-    version='0.0.1',
+    version='0.1',
     description="A Django app that provides generic per-object-permissions for Django's auth app.",
-    long_description=open('README').read(),
+    long_description=read('README'),
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
+    license='BSD',
     url='http://bitbucket.org/jezdez/django-authority/',
+    download_url='http://bitbucket.org/jezdez/django-authority/downloads/',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
     classifiers=[
@@ -22,6 +28,8 @@ setup(
     package_data = {
         'authority': [
             'templates/authority/*.html',
+            'templates/admin/edit_inline/action_tabular.html',
+            'templates/admin/permission_change_form.html',
         ]
     },
     zip_safe=False,
