9dbc84f6fb84820d68d1266666ebd66007bd6854
Jason Ward <jason.ward@policystat.com>
2013-06-13 10:24:20 -0400

    bumped to version 0.6, update note

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='django-authority',
-    version='0.5',
+    version='0.6',
     description=(
         "A Django app that provides generic per-object-permissions "
         "for Django's auth app."
