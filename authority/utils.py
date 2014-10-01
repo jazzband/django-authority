611d9167f7a69ed8fcc8d9c876fce6999d9b6629
joke2k <joke2k@gmail.com>
2014-10-01 13:38:16 +0200

    add gitignore and fix utils.get_user_model

--- authority/utils.py
+++ authority/utils.py
@@ -3,9 +3,7 @@ from django.contrib import auth
 
 
 def get_user_class():
-    if hasattr(settings, "AUTH_USER_MODEL"):
-        return settings.AUTH_USER_MODEL
-    elif hasattr(auth, "get_user_model"):
+    if hasattr(auth, "get_user_model"):
         return auth.get_user_model()
     else:
         return auth.models.User
